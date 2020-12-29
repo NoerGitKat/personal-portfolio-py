7ce9bbbf7c287d127a790927b34b27258f80d336
Noer Paanakker <noer@lightworksweb.nl>
2020-12-29 20:00:39 +0100

    added custom homepage and used logic to print out project info

--- resume/views.py
+++ resume/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import ResumeProject
 
-# Create your views here.
+
+def home(request):
+    # 1 Grab objects from DB
+    projects = ResumeProject.objects.all()
+
+    # 2 Render objects in template
+    return render(request, 'index.html', {'projects': projects})
