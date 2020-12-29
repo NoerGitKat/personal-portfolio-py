35df0cfc82435a115ee0cfc1a5004eb307c38a23
Noer Paanakker <noer@lightworksweb.nl>
2020-12-29 22:34:06 +0100

    created blog model and output list

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=30)
+    date = models.DateField()
+    description = models.CharField(max_length=200)
