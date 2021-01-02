c36e9d29e311beb769f20c212083b5314f96f8ed
Noer Paanakker <noer@lightworksweb.nl>
2021-01-02 15:45:43 +0100

    finished functionality of website

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,6 @@ class Blog(models.Model):
     title = models.CharField(max_length=30)
     date = models.DateField()
     description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title + " - " + self.description
